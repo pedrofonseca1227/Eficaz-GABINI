f2771d8280e8aa8160db4248aa4b34dec773a1ed
pedrofonseca1227 <henrriquemaluko@gmail.com>
2024-10-29 16:53:19 -0300

    att

--- crudd/obj/Debug/net8.0/crudd.AssemblyInfo.cs
+++ crudd/obj/Debug/net8.0/crudd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crudd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e83a4b24a5cc3fa51a7473e098fd184e2c351a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcb40ab8c61824034bc7745f7b74d58a6e6aa33")]
 [assembly: System.Reflection.AssemblyProductAttribute("crudd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crudd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
